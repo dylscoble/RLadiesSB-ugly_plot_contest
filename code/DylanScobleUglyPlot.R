ba6659d07d8faabe3f11d16aeb73b27843f71237
dylscoble <dscoble1226@gmail.com>
2022-01-26 16:32:57 -0800

    "Ugly Plot complete, I hope it is ugly enough."

--- code/DylanScobleUglyPlot.R
+++ code/DylanScobleUglyPlot.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 
-sweaters <- read_csv(here::here("data/use_this_data", "holiday_sweaters-2020-12-15-clean.csv"))
+sweaters <- read_csv(here::here("data/use_this_data", "holiday_sweaters-2020-12-15-clean.csv")) %>%
+  separate_rows(colors, sep = c(", ")) %>% 
+  group_by(hs_tf) %>% 
+  summarize(sum = length(colors))
 
-sweaters
+ggplot(data=sweaters, aes(x = hs_tf)) +
+  geom_bar()  +
+  labs(x = "Has a sweater?", 
+       y = "Total number of Entries") +
+  theme(strip.background = element_rect(fill = "blue"),
+        plot.background = element_rect(fill = "pink"),
+        legend.background = element_rect(fill = "orange"))
+
+ggsave(here::here("figures/an-plot.jpg"), width = 7, height = 4, dpi = 150)
