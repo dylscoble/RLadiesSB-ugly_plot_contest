bc851a60f2ddee206d570204cd4b11d0fd9da79e
dylscoble <dscoble1226@gmail.com>
2022-01-28 18:30:40 -0800

    Finished the assignment

--- code/DylanScobleUglyPlot.R
+++ code/DylanScobleUglyPlot.R
@@ -1,16 +1,22 @@
 library(tidyverse)
 
 sweaters <- read_csv(here::here("data/use_this_data", "holiday_sweaters-2020-12-15-clean.csv")) %>%
-  separate_rows(colors, sep = c(", ")) %>% 
-  group_by(hs_tf) %>% 
-  summarize(sum = length(colors))
+  group_by(hs_tf)
 
 ggplot(data=sweaters, aes(x = hs_tf)) +
-  geom_bar()  +
+  geom_bar(fill="#eebb00") +
+  facet_wrap(~colors) +
   labs(x = "Has a sweater?", 
-       y = "Total number of Entries") +
-  theme(strip.background = element_rect(fill = "blue"),
-        plot.background = element_rect(fill = "pink"),
+       y = "Total number of Entries",
+       title = "How many people in this dataset have sweaters, by color:") +
+  theme(plot.title = element_text(face = "bold", color="#ffa800"),
+        panel.grid = element_line(color="#00ff00"),
+        axis.title = element_text(color="#ffa000"),
+        axis.text = element_text(color="#ff8800"),
+        panel.background = element_rect(fill = "#ffc000"),
+        plot.background = element_rect(fill = "#ffbb00"),
+        strip.background = element_rect(fill = "#945300"),
+        strip.text = element_text(color = "#8c6f0f"),
         legend.background = element_rect(fill = "orange"))
 
 ggsave(here::here("figures/an-plot.jpg"), width = 7, height = 4, dpi = 150)
